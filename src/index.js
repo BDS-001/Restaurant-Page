305bbd45715507b9795a99754bafc0fc75b47308
BDS <113209967+BDS-001@users.noreply.github.com>
2023-11-02 23:34:41 -0400

    setup modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 console.log('Inside of src/index.js write a simple console.log or alert statement')
+
+import initialLoad from "./load-page"
+import restaurantElements from "./webpage-elements"
---
+++ src/webpage-elements.js
@@ -0,0 +1,9 @@
+
+
+export default function restaurantElements() {
+    const webpage = document.querySelector('#content')
+
+    const header = document.createElement('h1')
+    header.innerHTML = 'The Code & Byte Bistro Grand Opening!'
+
+}
