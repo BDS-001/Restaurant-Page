8363c876094825d77a56b5f0771038cc27325aee
BDS <113209967+BDS-001@users.noreply.github.com>
2023-11-10 22:30:31 -0500

    added content

--- src/about.js
+++ src/about.js
@@ -5,6 +5,7 @@ export const about = (function() {
     const renderAbout = function() {
         restaurant.innerHTML = ''
         const aboutContainer = document.createElement('div')
+        aboutContainer.className = 'about-container'
 
         const tmp = document.createElement('p')
         tmp.innerHTML = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis, expedita quisquam minus iusto quo at, suscipit ea, odit amet quia rem est ut hic consequuntur error. Molestiae quasi id maxime.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis, expedita quisquam minus iusto quo at, suscipit ea, odit amet quia rem est ut hic consequuntur error. Molestiae quasi id maxime.'
@@ -16,16 +17,20 @@ export const about = (function() {
     const _contact = function() {
         const contact = document.createElement('div')
 
+        const title = document.createElement('h1')
+        title.innerHTML = 'Contact Information'
+        contact.append(title)
+
         const phone = document.createElement('h3')
-        phone.innerHTML = '999-999-9999'
+        phone.innerHTML = 'Phone: 999-999-9999'
         contact.append(phone)
 
         const email = document.createElement('h3')
-        email.innerHTML = 'contact@bitandbytebistro.com'
+        email.innerHTML = 'Email: contact@bitandbytebistro.com'
         contact.append(email)
 
         const address = document.createElement('h3')
-        address.innerHTML = '123 realstreet rd'
+        address.innerHTML = 'Address: 123 realstreet rd'
         contact.append(address)
 
         return contact
